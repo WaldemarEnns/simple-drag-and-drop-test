f8f5d1f447b201c121113a85efc0c1ee80659707
Waldemar Enns <enux@MBPvonWaldemar.fritz.box>
2023-04-12 22:08:20 +0200

    Refactors dropzone logic to add support for returning the elements into the draggable-container

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const draggables = document.querySelectorAll('.draggable');
     const dropzones = document.querySelectorAll('.dropzone');
     const storeDropzonesBtn = document.getElementById('storeDropzones');
+    const draggableContainer = document.querySelector('.draggable-container');
 
     // Adding event-listeners for the numbers that are being dragged
     // This basically just stores the value of the number that is being dragged
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Adding event-listeners for the dropzones
     // Some of them just change the color of the dragged number and the dropzone - no further funcitonality
-    dropzones.forEach(dropzone => {
+    function setupDropzone(dropzone) {
         // Preventing the default behaviour of the browser
         dropzone.addEventListener('dragover', function (event) {
             event.preventDefault();
@@ -34,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // this check makes sure that the dragenter event is only applied on the dropzone
             // and not on the numbers
             // This prevents numbers from being dragged into numbers
-            if (!event.target.classList.contains('dropzone')) {
+            if (!event.target.classList.contains('dropzone') && !event.target.classList.contains('draggable-container')) {
                 return;
             }
 
@@ -42,40 +43,54 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         dropzone.addEventListener('dragleave', function (event) {
-            if (!event.target.classList.contains('dropzone')) {
+            if (!event.target.classList.contains('dropzone') && !event.target.classList.contains('draggable-container')) {
                 return;
             }
 
-            event.target.style.backgroundColor = 'lightgrey';
+            event.target.style.backgroundColor = dropzone.classList.contains('dropzone') ? 'lightgrey' : '';
         });
 
         // This handler is responsible for the actual dropping of the number
         // It stores a number in a dropzone and removes an existing number from the dropzone, if
         // there is one
         dropzone.addEventListener('drop', function (event) {
-            if (!event.target.classList.contains('dropzone')) {
+            if (!event.target.classList.contains('dropzone') && !event.target.classList.contains('draggable-container')) {
                 return;
-            }            
+            }                        
 
             event.preventDefault();
-            event.target.style.backgroundColor = 'lightgrey';
+            event.target.style.backgroundColor = dropzone.classList.contains('dropzone') ? 'lightgrey' : '';
 
-            // check if there is already a number stored
-            const previousElement = event.target.querySelector('.draggable');
+            if (event.target.classList.contains('draggable-container')) {
+                event.target.appendChild(dragged);
+                dragged.style.width = '50px';
+                dragged.style.height = '50px';
+                dragged.style.lineHeight = '50px';
+            } else {
+                // check if there is already a number stored
+                const previousElement = event.target.querySelector('.draggable');
 
-            // if there is a number stored, remove it from the dropzone
-            // and return it back to the draggable-container, where all the available numbers
-            // are stored
-            if (previousElement) {
-                const parent = document.querySelector('.draggable-container');
-                previousElement.style.backgroundColor = 'lightblue';
-                parent.appendChild(previousElement);
+                // if there is a number stored, remove it from the dropzone
+                // and return it back to the draggable-container, where all the available numbers
+                // are stored
+                if (previousElement) {
+                    const parent = document.querySelector('.draggable-container');
+                    previousElement.style.backgroundColor = 'lightblue';
+                    parent.appendChild(previousElement);
+                }
+
+                // add the dropped number to the dropzone
+                event.target.appendChild(dragged);
             }
-
-            // add the dropped number to the dropzone
-            event.target.appendChild(dragged);
         });
-    });
+    }
+
+    // Setup dropzones
+    dropzones.forEach(setupDropzone);
+
+    // Setup draggable-container as a dropzone
+    setupDropzone(draggableContainer);
+
 
     // This handler is responsible for storing the numbers in the dropzones
     // and emitting a POST request to the server
